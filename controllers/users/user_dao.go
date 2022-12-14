9cac98058fe7cf89e541925f0d6f59770e8e32a2
Rozia Williams <roziawilliams@gmail.com>
2022-12-14 17:03:32 -0500

    User dao

--- controllers/users/user_dao.go
+++ controllers/users/user_dao.go
@@ -1,1 +1,18 @@
 package users
+
+import (
+	"fmt"
+	"github.com/roziawilliams/bookstore_users-api/domain/users"
+	"github.com/roziawilliams/bookstore_users-api/utils/errors"
+)
+
+func (user User) Get() *errors.RestErr {
+	i := users.User{Id: 125}
+	fmt.Println(i)
+	return nil
+}
+
+func (user User) Save() *errors.RestErr {
+
+	return nil
+}
